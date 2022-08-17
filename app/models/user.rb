bece046f6c2e9226d85e0e0d01fc498435d9c517
Marcel Fonseca <marcel@lewagon.org>
2022-08-17 20:40:40 +0200

    Add cloudinary access and usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :wares, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :wares, through: :bookings
+  has_many :booked_wares, through: :bookings
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+# file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+# article = Article.new(title: "NES", body: "A great console")
+# article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+# article.save
+
+
+
 puts "Cleaning the Database"
 User.destroy_all
 Ware.destroy_all
@@ -18,10 +27,37 @@ gm = User.create(name: 'Gimli', c_class: 'Warrior', password: 'melon123', email:
 l = User.create(name: 'Legolas', c_class: 'Archer', password: 'melon123', email: 'perfect@gmail.com')
 
 puts "Generating legendary wares"
-w1 = Ware.create(name: 'Narsil', description: 'The sword of the king of Gondor and Arnor', price: 100, user: a, picture_url: '')
-w2 = Ware.create(name: 'Bow of Legolas', description: 'You cannot miss with this bow!', price: 100, user: l, picture_url: '')
-w3 = Ware.create(name: 'The one Ring', description: 'One Ring to rule them all', price: 100, user: b, picture_url: '')
-w5 = Ware.create(name: 'Sting', description: 'More like a knife than a proper sword', price: 100, user: b, picture_url: '')
-w4 = Ware.create(name: 'Glamdring', description: 'The legendary Foehammer of the former king of Gondolin', price: 100, user: g, picture_url: '')
-w6 = Ware.create(name: 'Gandalfs staff', description: 'The staff of the greatest wizard of all time', price: 100, user: g, picture_url: '')
+w1_file = URI.open("https://cdnb.artstation.com/p/assets/images/images/024/229/907/large/cleber-moretti-narsil-01-ed.jpg?1581943382")
+w1 = Ware.create(name: 'Narsil', description: 'The sword of the king of Gondor and Arnor', price: 250, user: a, picture_url: '')
+w1.photos.attach(io: w1_file, filename: 'narsil.png', content_type: 'image/png')
+w1.save
 
+w2_file = URI.open("https://i.pinimg.com/736x/ff/e0/90/ffe090dfa9eb6816e06ef27e7ebe94df.jpg")
+w2 = Ware.create(name: 'Bow of Legolas', description: 'You cannot miss with this bow!', price: 200, user: l, picture_url: '')
+w2.photos.attach(io: w2_file, filename: 'bow.png', content_type: 'image/png')
+w2.save
+
+w3_file = URI.open("https://images.squarespace-cdn.com/content/v1/59eb38af8c56a88932c45631/1556629944066-12XYONY27XKLNXU5BJVL/The-One-Ring-Web.jpg?format=1000w")
+w3 = Ware.create(name: 'The one Ring', description: 'One Ring to rule them all', price: 1000000, user: b, picture_url: '')
+w3.photos.attach(io: w3_file, filename: "theone.jpeg", content_type: "image/jpg")
+w3.save
+
+w5_file = URI.open("https://cdna.artstation.com/p/assets/images/images/005/868/906/large/chase-miller-swords.jpg?1494349335")
+w5 = Ware.create(name: 'Sting', description: 'More like a knife than a proper sword', price: 100, user: b, picture_url: '')
+w5.photos.attach(io: w5_file, filename: "sting.png", content_type: "image/png")
+w5.save
+
+w4_file = URI.open("https://cdn.reliks.com/products/5428/1200x0/glamdring-hilt.jpg")
+w4 = Ware.create(name: 'Glamdring', description: 'The legendary Foehammer of the former king of Gondolin', price: 350, user: g, picture_url: '')
+w4.photos.attach(io: w4_file, filename: "glamdring.jpeg", content_type: "image/jpg")
+w4.save
+
+w6_file = URI.open("https://cdnb.artstation.com/p/assets/images/images/013/564/643/large/ruben-dias-pau6.jpg?1540200200")
+w6 = Ware.create(name: 'Gandalfs staff', description: 'The staff of the greatest wizard of all time', price: 50, user: g, picture_url: '')
+w6.photos.attach(io: w6_file, filename: "staff.png", content_type: "image/png")
+w6.save
+
+w7_file = URI.open("https://www.outfit4events.de/runtime/cache/images/redesignProductFull/uc1298.jpg")
+w7 = Ware.create(name: 'Hadhafang', description: 'The perfect elven princess blade', price: 300, user: gm, picture_url: '')
+w7.photos.attach(io: w7_file, filename: "hadhafang.jpeg", content_type: "image/jpg")
+w7.save
