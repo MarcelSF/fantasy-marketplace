0caf1c04e9a3aa6afe79ab4f0380a054609bdc19
Marcel Fonseca <marcel@lewagon.org>
2022-08-17 10:42:33 +0200

    Add initial view files

--- app/controllers/wares_controller.rb
+++ app/controllers/wares_controller.rb
@@ -1,22 +1,28 @@
 class WaresController < ApplicationController
-  def index
+  before_action :set_ware, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @wares = Ware.all
   end
 
   def show
-
   end
 
   def new
-
+    @ware = Ware.new
   end
 
   def create
-
+    @ware = Ware.new(ware_params)
+    @ware.user = current_user
+    if ware.save
+      redirect_to ware
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
@@ -24,6 +30,13 @@ class WaresController < ApplicationController
   end
 
   def destroy
+    @ware.destroy
+    redirect_to wares_path
+  end
 
+  private
+
+  def set_ware
+    @ware = Ware.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning the Database"
+
+puts "Generating legendary users"
+
+puts "Generating legendary wares"
