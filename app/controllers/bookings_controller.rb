4237a74e5cbb97448bd04cd475bfe43e91758f53
Marcel Fonseca <marcel@lewagon.org>
2022-08-21 20:00:46 +0200

    add bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,16 @@
+class BookingsController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date)
+
+  end
+end
