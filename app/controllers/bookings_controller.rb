3ef150c8c92c36acfa5def255e58d0054e4bb57d
Marcel Fonseca <marcel@lewagon.org>
2022-08-22 14:32:45 +0200

    Add time difference and booking controller with dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,19 @@
 class BookingsController < ApplicationController
-  def index
-
-  end
-
   def create
-
+    @booking = Booking.new(bookings_params)
+    @booking.user = current_user
+    @ware = Ware.find(params[:ware_id])
+    @booking.ware = @ware
+    if @booking.save
+      redirect_to dashboards_path
+    else
+      render "wares/show"
+    end
   end
 
   private
 
   def bookings_params
-    params.require(:booking).permit(:start_date, :end_date)
-
+    params.require(:booking).permit(:start_time, :end_time)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :ware
+
+  validates :start_time, :end_time, presence: true
+
+  scope :my_bookings, -> { where(user: current_user) }
+
+  def total_price
+    TimeDifference.between(start_time, end_time).in_days * ware.price
+  end
 end
