b8eb795cc710113a8cdeb787b8a01ef9b0d37c33
Marcel Fonseca <marcel@lewagon.org>
2022-08-23 09:49:41 +0200

    Add bookings logic

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,9 @@
 class DashboardsController < ApplicationController
-  def bookings_manager
+  def bookings_dashboard
     @bookings = current_user.bookings
+  end
+
+  def booking_requests_dashboard
+    @bookings = Booking.where(ware_id: current_user.wares.pluck(:id).flatten)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'bookings_manager', to: 'dashboards#bookings_manager', as: :dashboards
+  get 'bookings_manager', to: 'dashboards#bookings_dashboard', as: :dashboards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
